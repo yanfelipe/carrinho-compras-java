07a80b9049957906c581ca1a2d1a5f7e19d99269
willferrero <willferrero@487fb80c-313c-283c-8a53-a74e9dc98d77>
2013-05-29 03:41:41 +0000

    Início do projeto. Montagem da estrutura, inserção das bibliotecas e configuração do JSF (web.xml e faces-config.xml). Criação da entidade Cliente e arquivos de mensagens.

---
+++ loja/src/br/com/javamagazine/entity/Cliente.java
@@ -0,0 +1,67 @@
+package br.com.javamagazine.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String nome;
+	
+	@Column(nullable=false, unique=true)
+	private String email;
+	
+	@Column(nullable=false)
+	private String senha;
+
+	//getters e setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
